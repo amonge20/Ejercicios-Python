2e0a895b1f522e87685d5a5a398a8ace7d56e322
amonge20 <2023_aitor.monge@iticbcn.cat>
2025-09-15 13:15:32 +0200

    Script de Python, cambio de cambios de EUR a USD (ahora con tasa de gestion)

--- casa_de_cambios.py
+++ casa_de_cambios.py
@@ -1,9 +1,14 @@
 # Numero del dolar
 USD = 1.20
+# tasa de gestion
+tasa_de_gestion = 10
 
 # Preguntamos la cantidad de dinero al usuario
 convertidor_moneda = float(input("Introduce la cantidad de 'EUR' y luego despues se mostrara la cantidad de 'EUR' en 'USD': "))
-# Calcula el resultado dela cantidad de EUR pedido + rondedeamos el resultado 
-resutado = convertidor_moneda * USD
-resutado = round(resutado,2)
-print(convertidor_moneda,"€, en dolares se convierte en $",resutado)
+# La casa de cambios se queda un 10% en concepto de 'tasas de gestion'
+resutado_con_tasa_de_gestion = convertidor_moneda * (USD / tasa_de_gestion)
+resutado_con_tasa_de_gestion = round(resutado_con_tasa_de_gestion,2)
+resutado_con_usuario = convertidor_moneda - resutado_con_tasa_de_gestion
+resutado_con_usuario = round(resutado_con_usuario,2)
+print("Tasa de gestion: ",resutado_con_tasa_de_gestion)
+print("El monto que recibe el usuario: ",resutado_con_usuario)
